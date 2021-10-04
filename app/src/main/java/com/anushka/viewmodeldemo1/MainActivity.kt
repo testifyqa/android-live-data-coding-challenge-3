6d72c84f8d6479288451c9bfb08c1ecdc466f0df
Thomas Knee <tom@testifyqa.com>
2021-10-04 10:00:56 +0100

    complete live data coding challenge

---
+++ app/src/main/java/com/anushka/viewmodeldemo1/MainActivity.kt
@@ -0,0 +1,25 @@
+package com.anushka.viewmodeldemo1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.ViewModelProvider
+import com.anushka.viewmodeldemo1.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var viewModel: MainActivityViewModel
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        viewModel = ViewModelProvider(this).get(MainActivityViewModel::class.java)
+        viewModel.countData.observe(this, {
+            binding.countText.text = it.toString()
+        })
+
+        binding.button.setOnClickListener {
+            binding.countText.text = viewModel.getUpdatedCount().toString()
+        }
+    }
+}
